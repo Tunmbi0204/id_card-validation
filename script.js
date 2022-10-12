93454324d4f303e7dadbed0561261915fbe27d45
TUNMBI <adelowotumbi1@gmail.com>
2022-10-12 15:06:23 +0200

    completing  my validation

--- script.js
+++ script.js
@@ -6,16 +6,37 @@ let regForm = document.getElementById('registerform');
    let Age = regForm['Age'].value;
    let phonenumber = regForm['phonenumber'].value;
 
+//    name
    let myname = document.getElementById("myname");
    myname.innerHTML = name;
 
+//    email 
    let myemail = document.getElementById("myemail");
    myemail.innerHTML = email;
 
+//    age
    let age = document.getElementById("age");
-   age.innerHTML = Age;
+   if( Age >= 18){
+     valid = Age
+   }
+else{
+    valid = "You must be " + 18
+}
+      age.innerHTML = valid;
 
+
+    // phonenumber
    let inputnumber = document.getElementById("inputnumber");
-   inputnumber.innerHTML = phonenumber;
+
+   if(phonenumber.startsWith('070') || phonenumber.startsWith('080')  || phonenumber.startsWith('081') || phonenumber.startsWith('090'))
+   if(phonenumber.length == 11){
+      valid = phonenumber} 
+      else{
+  valid = "Number is Invalid"}  
+  else{
+   valid = "Number is Invalid"}
+  inputnumber.innerHTML = valid;
+   
+
 
 })
